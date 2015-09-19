69325547f6a95cd44f9a7313efb9ec683d905b98
ycw <1064832651@qq.com>
2015-09-19 18:08:29 +0800

    22

--- app/src/main/java/ruanyun/com/criminalintent2/Crime.java
+++ app/src/main/java/ruanyun/com/criminalintent2/Crime.java
@@ -1,18 +1,31 @@
 package ruanyun.com.criminalintent2;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by Administrator on 2015/9/18.
  */
 public class Crime {
+    /**
+     * 属性
+     */
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
 
+    /**
+     * 构造函数
+     */
     public Crime() {
         mId = UUID.randomUUID();//自动生成Id
+        mDate = new Date();
     }
 
+    /**
+     * getting setting 方法
+     */
     public UUID getId() {
         return mId;
     }
@@ -23,5 +36,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
     }
 }
--- app/src/main/java/ruanyun/com/criminalintent2/CrimeFragment.java
+++ app/src/main/java/ruanyun/com/criminalintent2/CrimeFragment.java
@@ -3,6 +3,8 @@ package ruanyun.com.criminalintent2;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +15,14 @@ import android.widget.EditText;
  */
 public class CrimeFragment extends Fragment {
 
+    private String TAG = "CrimeFragment";
     private Crime mCrime;
+    private EditText edit_title;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
-
     }
 
     @Nullable
@@ -27,11 +30,27 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime, container, false);
-        EditText edit_title = (EditText) view.findViewById(R.id.crime_edit);
 
+        edit_title = (EditText) view.findViewById(R.id.crime_edit);
+        edit_title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//                Log.i(TAG, "beforeTextChanged:---->" + s.toString() +"    start:--->" + start +"    count:--->" + count +"    after:--->" + after);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                Log.i(TAG, "onTextChanged:---->" + s.toString() + "    start:--->" + start + "    before:--->" + before + "    count:--->" + count);
+                mCrime.setTitle(s.toString());//编辑框中的文字 设置为Crime标题
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+//                Log.i(TAG, "afterTextChanged:---->" + s.toString());
+            }
+        });
 
         return view;
     }
-
 
 }
